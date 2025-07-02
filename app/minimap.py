b40905460dccd7a0964e6cc8c35d1c04c05620a3
Fabian Ornelas <fabe.ornelas98@gmail.com>
2025-07-01 23:49:13 -0600

    Update minimap module for tile functionality.

--- app/minimap.py
+++ app/minimap.py
@@ -7,12 +7,14 @@ class FogOfWar:
         self.tile_size = tile_size
         self.explored = np.zeros(map_size, dtype=bool)
 
-    def update(self, player_pos, light_radius):
+    def update(self, player_tile, light_radius):
         for x in range(self.map_size[0]):
             for y in range(self.map_size[1]):
-                tile_pos = np.array([x * self.tile_size, y * self.tile_size])
-                if np.linalg.norm(player_pos - tile_pos) <= light_radius:
+                dx = x - player_tile[0]
+                dy = y - player_tile[1]
+                if dx * dx + dy * dy <= light_radius * light_radius:
                     self.explored[x, y] = True
+
 
     def draw_minimap(self, screen):
         for x in range(self.map_size[0]):
